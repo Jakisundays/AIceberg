8067d93a847b322a2c1133e4e34a014c1db3724c
Jakisundays <jakisundays@gmail.com>
2023-07-18 20:29:48 -0300

    fixed img

--- components/CelebrityCard.tsx
+++ components/CelebrityCard.tsx
@@ -1,6 +1,7 @@
 "use client";
 import useAuthModal from "@/hooks/useAuthModal";
 import { useUser } from "@/hooks/useUser";
+import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 
@@ -32,16 +33,25 @@ const CelebrityCard = ({
   return (
     <div className="card w-72 bg-neutral-400/5 hover:bg-neutral-400/10 transition shadow-xl">
       <figure className="px-10 pt-10">
-        <img src={image} alt="Shoes" className="rounded-xl" />
+        <Image
+          src={image}
+          alt="Artist"
+          width={500}
+          height={500}
+          className="rounded-xl"
+        />
       </figure>
       <div className="card-body items-center text-center">
         <h2 className="card-title">{name}</h2>
         <p>{description}</p>
-        {/* <Link href={href} className="card-actions mt-1"> */}
-        <button onClick={handleChat} disabled={disabled} className="btn btn-primary transition">
+
+        <button
+          onClick={handleChat}
+          disabled={disabled}
+          className="btn btn-primary transition"
+        >
           {disabled ? "Coming soon" : "Chat now"}
         </button>
-        {/* </Link> */}
       </div>
     </div>
   );
