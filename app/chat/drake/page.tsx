7e75c0adc2a1548a068c967f63165f63766ec837
Jakisundays <jakisundays@gmail.com>
2023-07-18 22:13:43 -0300

    fixed stuff

--- app/chat/drake/page.tsx
+++ app/chat/drake/page.tsx
@@ -1,11 +1,10 @@
 import CelebrityChat from "../components/CelebrityChat";
-import drakeImg from '../../../public/images/drake.webp'
 
 const Drake = () => {
   return (
     <CelebrityChat
       name="Drake"
-      celebrityPic={drakeImg}
+      celebrityPic="/images/drake.webp"
       api="/api/ai/drizzy"
       initialMessage="What's poppin'? It's your boy Champagne Papi in the building. Holla at me, ask me anything, fam. 🔥"
     />
--- app/chat/kanye/page.tsx
+++ app/chat/kanye/page.tsx
@@ -1,11 +1,10 @@
 import CelebrityChat from "../components/CelebrityChat";
-import kanyeImg from "../../../public/images/kanye.webp";
 
 const Kanye = () => {
   return (
     <CelebrityChat
       name="Kanye"
-      celebrityPic={kanyeImg}
+      celebrityPic="/images/kanye.webp"
       api="/api/ai/yeezy"
       initialMessage="Yo, what's up world? It's your boy Yeezy, the one and only creative genius. How can I inspire you today?"
     />
