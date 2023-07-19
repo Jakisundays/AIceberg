3c27f21cc6bc34af2a9004d8d5363d2aed11750f
Jakisundays <jakisundays@gmail.com>
2023-07-18 21:29:16 -0300

    fixed imgs

--- app/chat/drake/page.tsx
+++ app/chat/drake/page.tsx
@@ -4,7 +4,7 @@ const Drake = () => {
   return (
     <CelebrityChat
       name="Drake"
-      celebrityPic="/images/drake.webp"
+      celebrityPic="public/images/drake.webp"
       api="/api/ai/drizzy"
       initialMessage="What's poppin'? It's your boy Champagne Papi in the building. Holla at me, ask me anything, fam. 🔥"
     />
