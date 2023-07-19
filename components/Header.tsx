108fff89c130aaa84392f60abb30cea8a79cb954
Jakisundays <jakisundays@gmail.com>
2023-07-18 22:38:58 -0300

    fixed stuff

--- components/Header.tsx
+++ components/Header.tsx
@@ -44,12 +44,14 @@ const Header = ({ children, className }: HeaderProps) => {
         <div className="hidden md:flex gap-x-2 items-center">
           <button
             onClick={() => router.back()}
+            aria-label="Back"
             className="rounded-full bg-black flex items-center justify-center hover:opacity-75 transition"
           >
             <RxCaretLeft className="text-white" size={35} />
           </button>
           <button
             onClick={() => router.forward()}
+            aria-label="Forward"
             className="rounded-full bg-black flex items-center justify-center hover:opacity-75 transition"
           >
             <RxCaretRight className="text-white" size={35} />
@@ -58,18 +60,21 @@ const Header = ({ children, className }: HeaderProps) => {
         <div className="flex md:hidden gap-x-2 items-center">
           <button
             onClick={() => router.push("/")}
+            aria-label="Home"
             className="rounded-full p-2 bg-white flex items-center justify-center hover:opacity-75 transition"
           >
             <HiHome className="text-black" size={20} />
           </button>
           <button
             onClick={() => router.push("/search")}
+            aria-label="Search"
             className="rounded-full p-2 bg-white flex items-center justify-center hover:opacity-75 transition"
           >
             <BiSearch className="text-black" size={20} />
           </button>
           <button
             onClick={() => router.push("/chat")}
+            aria-label="Chat"
             className="rounded-full p-2 bg-white flex items-center justify-center hover:opacity-75 transition"
           >
             <HiMiniChatBubbleLeft className="text-black" size={20} />
@@ -80,14 +85,12 @@ const Header = ({ children, className }: HeaderProps) => {
             <div className="flex gap-x-4 items-center">
               <Button
                 onClick={handleLogOut}
+                aria-label="Log out"
                 className="bg-white px-6 py-2 text-black"
               >
                 Logout
               </Button>
-              <Button
-                onClick={() => router.push("/account")}
-                className="bg-white text-slate-950"
-              >
+              <Button aria-label="Profile" className="bg-white text-slate-950">
                 <FaUserAlt />
               </Button>
             </div>
